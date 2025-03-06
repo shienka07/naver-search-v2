01e99965447cad39630ab5e2145f642047534748
qus0in <qus0in@gmail.com>
2025-03-06 16:11:02 +0900

    servlet 수정

--- src/main/java/controller/search/SearchController.java
+++ src/main/java/controller/search/SearchController.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.dto.NaverAPIResultItem;
 import service.search.SearchService;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
